7ccbe1b1ee7ad01c375ef1c53ebc1bdd4cda8b7e
tianao <t5meng@uwaterloo.ca>
2021-10-16 18:39:46 -0400

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Blogs from '../views/Blogs.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import ForgotPassword from '../views/ForgotPassword.vue'
+import Profile from '../views/Profile.vue'
+import Admin from '../views/Admin.vue'
+import CreatePost from '../views/CreatePost.vue'
+import BlogPreview from '../views/BlogPreview.vue'
+import ViewBlog from '../views/ViewBlog.vue'
+import EditBlog from '../views/EditBlog.vue'
+import firebase from "firebase/app";
+import "firebase/auth";
 
 Vue.use(VueRouter)
 
@@ -8,22 +20,143 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta:{
+      title: 'Home',
+      requiresAuth:false,
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/blogs',
+    name: 'Blogs',
+    component: Blogs,
+    meta:{
+      title: 'Blogs',
+      requiresAuth:false,
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta:{
+      title: 'Login',
+      requiresAuth:false,
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    meta:{
+      title: 'Register',
+      requiresAuth:false,
+    }
+  },
+  {
+    path: '/forgot-password',
+    name: 'ForgotPassword',
+    component: ForgotPassword,
+    meta:{
+      title: 'Forgot Password',
+      requiresAuth:false,
+    }
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    meta:{
+      title: 'Profile',
+      requiresAuth:true,
+    }
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    meta:{
+      title: 'Admin',
+      requiresAuth:true,
+      requiresAdmin:true,
+    }
+  },
+  {
+    path: '/create-post',
+    name: 'CreatePost',
+    component: CreatePost,
+    meta:{
+      title: 'Create Post',
+      requiresAuth:true,
+      requiresAdmin:true,
+    }
+  },
+  {
+    path: '/blog-preview',
+    name: 'BlogPreview',
+    component: BlogPreview,
+    meta:{
+      title: 'Blog Preview',
+      requiresAuth:true,
+      requiresAdmin:true,
+    }
+  },
+
+  {
+    path: '/view-blog/:blogId',
+    name: 'ViewBlog',
+    component: ViewBlog,
+    meta:{
+      title: 'View Blog',
+      requiresAuth:false,
+    }
+  },
+
+  {
+    path: '/edit-blog/:blogId',
+    name: 'EditBlog',
+    component: EditBlog,
+    meta:{
+      title: 'Edit Blog',
+      requiresAuth:true,
+      requiresAdmin:true,
+    }
+  },
+
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
+router.beforeEach((to, from, next) =>{
+  document.title = `${to.meta.title} | FireBlog`;
+  next();
+});
+
+router.beforeEach(async (to, from, next) =>{
+    let user = firebase.auth().currentUser;
+    let admin = null;
+    if(user){
+      let token = await user.getIdTokenResult();
+      admin = token.claims.admin;
+    }
+    console.log("to: ", to);
+    console.log("to.matched: ", to.matched);
+    if(to.matched.some((res) => res.meta.requiresAuth)){
+      if(user){
+        if(to.matched.some((res) => res.meta.requiresAdmin)){
+          if(admin){
+            return next();
+          }
+          return next({name:"Home"});
+        }
+        return next();
+      }
+      return next({name:"Home"});
+    }
+    return next();
+});
 export default router
